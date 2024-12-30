6cb8610a8ab18e677faf3bd8bbe6e1c1f9fb024e
satyam-trimale <162133170+satyam-trimale@users.noreply.github.com>
2024-12-30 23:46:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,37 +17,47 @@ const winPattern = [
     [2,4,6],
     [3,4,5],
     [6,7,8]
-
 ];
+
 const resetGame = () => {
     turn0 = true;
+    count = 0;
     enableBoxes();
     msgContainer.classList.add("hide");
 }
+
 boxes.forEach((box) => {
-    box.addEventListener("click" , () =>{
+    box.addEventListener("click", () => {
         if(turn0){
-            //Player 0
-            
             box.innerText = "O";
             box.style.color = "#736372";
             turn0 = false;
-
-        }else{
+        } else {
             box.innerText = "X"
             box.style.color = "#A18276"
             turn0 = true
         }
         box.disabled = true;
         count++;
-        checkWinner();
+        
+        // First check for winner
+        let isWinner = checkWinner();
+        
+        // Only check for tie if there's no winner
+        if(!isWinner && count === 9){
+            msg.innerText = `It's a tie`;
+            msgContainer.classList.remove("hide");
+            disableBoxes();
+        }
     });
 });
+
 const disableBoxes = () => {
     for(let box of boxes){
         box.disabled = true;
     }
 }
+
 const enableBoxes = () => {
     for(let box of boxes){
         box.disabled = false;
@@ -56,15 +66,11 @@ const enableBoxes = () => {
 }
 
 const ShowWinner = (winner) => {
-    if(count == 9){
-        msg.innerText = `It's a tie`;
-    }else{
     msg.innerText = `Congratulations, Winner is ${winner}`;
-    }
     msgContainer.classList.remove("hide");
     disableBoxes();
-
 }
+
 const checkWinner = () => {
     for(let pattern of winPattern){
         let pos1Val = boxes[pattern[0]].innerText;
@@ -72,13 +78,15 @@ const checkWinner = () => {
         let pos3Val = boxes[pattern[2]].innerText;
 
         if(pos1Val != "" && pos2Val != "" && pos3Val != ""){ 
-        if(pos1Val === pos2Val && pos2Val === pos3Val){
-            console.log("Winner",pos1Val);
-            ShowWinner(pos1Val);
-
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                console.log("Winner", pos1Val);
+                ShowWinner(pos1Val);
+                return true; // Return true to indicate we found a winner
+            }
         }
     }
-    }
-};
-newGameBtn.addEventListener("click",resetGame);
-resetBtn.addEventListener("click",resetGame);
+    return false; // Return false if no winner found
+}
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
